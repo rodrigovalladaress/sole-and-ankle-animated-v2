5ac1f020f759536511246abbefe1c5b0578fb81f
Rodrigo <dim3trodon@gmail.com>
2025-11-13 11:29:29 +0000

    feat: add stretch goal for navigation links animation

--- src/components/Header/NavLink.jsx
+++ src/components/Header/NavLink.jsx
@@ -5,27 +5,39 @@ import { WEIGHTS } from "../../constants";
 const NavLink = ({ href, children }) => (
   <Link href={href}>
     <ChildrenWrapper>
-      <span>{children}</span>
+      <Child>{children}</Child>
       <BoldChild>{children}</BoldChild>
     </ChildrenWrapper>
   </Link>
 );
 
-const BoldChild = styled.span`
+const Child = styled.span`
+  transition: transform 350ms, opacity 650ms;
+  display: inline-block;
+  border: 1px solid var(--color-gray-900);
+  padding: 0px 6px;
+  /* Add a bit of margin so the borders are shown */
+  margin: 2px;
+`;
+
+const BoldChild = styled(Child)`
+  /* Hide the bold child as some artifacts appear when the child is rotated */
+  opacity: 0;
   font-weight: ${WEIGHTS.bold};
   position: absolute;
   left: 0;
-  top: 100%;
+  top: calc(100%);
+  transform-origin: center top;
+  transform: rotateX(-0.25turn);
 `;
 
 const ChildrenWrapper = styled.span`
-  display: inline-block;
+  display: block;
   position: relative;
   will-change: transform;
-
-  @media (prefers-reduced-motion: no-preference) {
-    transition: transform 250ms;
-  }
+  perspective: 100px;
+  transform-style: preserve-3d;
+  overflow: hidden;
 `;
 
 const Link = styled.a`
@@ -35,13 +47,20 @@ const Link = styled.a`
   color: var(--color-gray-900);
   font-weight: ${WEIGHTS.medium};
   overflow: hidden;
+  display: block;
 
   &:first-of-type {
     color: var(--color-secondary);
   }
 
-  &:hover ${ChildrenWrapper} {
-    transform: translateY(-100%);
+  &:hover ${Child} {
+    transform: translateY(-100%) rotateX(0.25turn);
+  }
+
+  &:hover ${BoldChild} {
+    opacity: 1;
+    transform: translateY(calc(-100% - 4px)) rotateX(0turn);
+    transition: transform 250ms, opacity 50ms;
   }
 `;
 
